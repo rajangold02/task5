43c1de4309c4deabe8465061b222ff14a656679a
rajangold02 <rajangold02@gmail.com>
2018-09-16 12:12:33 -0400

    changes for lambda

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_cloudtrail" "s3_cloudtrail" {
 
     data_resource {
       type   = "AWS::S3::Object"
-	  values = ["${aws_s3_bucket.s3_source_bucket.arn/}"]
+	  values = ["${aws_s3_bucket.s3_source_bucket.arn}/"]
     }
   }
 }
@@ -93,12 +93,18 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+data "archive_file" "lambda_zip" {
+    type        = "zip"
+    source_dir  = "function.py"
+    output_path = "function.zip"
+}
+
 resource "aws_lambda_function" "test_lambda" {
   filename         = "function.zip"
-  function_name    = "${function.lambda}"
+  function_name    = "function.lambda"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "function.sample"
-  source_code_hash = "${base64sha256(file("function.zip"))}"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
   runtime          = "python2.7"
 }
 resource "aws_iam_role_policy" "lambda_policy" {
